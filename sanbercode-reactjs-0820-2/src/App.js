995b62fddf00927be4cba2e0824c7f2ccb0c690f
andriananugrahputra <andriananugrah.putra@gmail.com>
2020-09-05 05:49:09 +0700

    update tugas 10

---
+++ sanbercode-reactjs-0820-2/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import FormPembelianBuah from "./Tugas-9/tugas9";
+import TabelHargaBuah from "./Tugas-10/tugas10";
+
+function App() {
+  return (
+    <div>
+      <FormPembelianBuah/>
+      <TabelHargaBuah/>
+    </div>
+  );
+}
+
+export default App;
---
+++ sanbercode-reactjs-0820-2/src/Tugas-9/tugas9.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './tugas9.css';
+
+class FormPembelianBuah extends React.Component {
+    render() {
+        return (
+            <div class="div-container">
+                <h1>Form Pembelian Buah</h1>
+                <div style={{display: 'block', marginBottom: '1em'}}>
+                    <span>
+                        Nama Pelanggan
+                    </span>
+                    <input style={{display: 'inline-block'}} type="text" name="name" />
+                </div>
+                <span>
+                    Daftar Item
+                </span>
+                <div style={{display: 'inline-block', marginBottom: '1em'}}>
+                    <input type="checkbox" name="item" />
+                    <label>Semangka</label><br />
+                    <input type="checkbox" name="item" />
+                    <label>Jeruk</label><br />
+                    <input type="checkbox" name="item" />
+                    <label>Nanas</label><br />
+                    <input type="checkbox" name="item" />
+                    <label>Salak</label><br />
+                    <input type="checkbox" name="item" />
+                    <label>Anggur</label><br />
+                </div>
+                <div>
+                    <button style={{background: 'white', borderRadius: '25px'}}> 
+                        <a href="/#" style={{color: 'black', textDecoration: 'none'}}>Kirim</a>
+                    </button>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default FormPembelianBuah;
