d18dc3cc56a0516da68fbb09ba77b72eec707ee4
andriananugrahputra <andriananugrah.putra@gmail.com>
2020-09-09 07:52:21 +0700

    update tugas 12

--- sanbercode-reactjs-0820-2/src/App.js
+++ sanbercode-reactjs-0820-2/src/App.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import FormPembelianBuah from "./Tugas-9/tugas9";
 import TabelHargaBuah from "./Tugas-10/tugas10";
-import Timer from "./Tugas-11/timer"
+import Timer from "./Tugas-11/timer";
+import TabelHargaBuah2 from "./Tugas-12/tabel_buah2";
 
 function App() {
   return (
     <div>
-      <FormPembelianBuah/>
-      <TabelHargaBuah/>
-      <Timer/>
+      <FormPembelianBuah />
+      <TabelHargaBuah />
+      <Timer />
+      <TabelHargaBuah2 />
     </div>
   );
 }
